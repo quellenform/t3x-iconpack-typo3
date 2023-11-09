60791545d290f83b0ea26a6b884fcb97ae5b75b2
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2023-11-09 21:39:04 +0100

    Fixed typos

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,19 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => 'Iconpack: TYPO3',
-    'description' => 'This Extension registers an Iconpack-Provider for EXT:iconpack which allows you to use the "TYPO3 Icons" in TYPO3.',
+    'title' => 'Iconpack: TYPO3 Icons',
+    'description' => 'This Extension registers an Iconpack-Provider for EXT:iconpack which allows you to use the "TYPO3 Icons" in ...TYPO3 (surprise!).',
     'category' => 'fe',
     'state' => 'beta',
     'clearcacheonload' => true,
     'author' => 'Stephan Kellermayr',
     'author_email' => 'typo3@quellenform.at',
     'author_company' => 'Kellermayr KG',
-    'version' => '0.0.1',
+    'version' => '0.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.11-12.4.99',
-            'iconpack' => '1.0.2-1.99'
+            'iconpack' => '1.1.0-1.99'
         ],
         'conflicts' => [],
         'suggests' => []
