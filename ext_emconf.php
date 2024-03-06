e081524b52cb8c5de6fba4b224b4c3da3f6c2f02
Stephan Kellermayr <stephan.kellermayr@gmail.com>
2024-03-06 20:11:18 +0100

    Added compatibility to TYPO3 v13

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,10 +9,10 @@ $EM_CONF[$_EXTKEY] = [
     'author' => 'Stephan Kellermayr',
     'author_email' => 'typo3@quellenform.at',
     'author_company' => 'Kellermayr KG',
-    'version' => '0.0.2',
+    'version' => '0.0.3',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.11-12.4.99',
+            'typo3' => '10.4.11-13.9.99',
             'iconpack' => '1.1.0-1.99'
         ],
         'conflicts' => [],
